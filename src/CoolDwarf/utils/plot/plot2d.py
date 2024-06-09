ddbc3c2356ed02c8c9a96d80103935dcfafbd998
Emily Boudreaux <emily@boudreauxmail.com>
2024-06-09 20:31:33 +0000

    refactor(plot2d.py): plot2d uses spherical coordinates now

--- src/CoolDwarf/utils/plot/plot2d.py
+++ src/CoolDwarf/utils/plot/plot2d.py
@@ -2,22 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def visualize_scalar_field(scalar_field, slice_axis='z', slice_index=None, **kwargs):
-    if slice_axis not in ['x', 'y', 'z']:
-        raise ValueError("slice_axis must be 'x', 'y', or 'z'")
+    if slice_axis not in ['i', 'j', 'k']:
+        raise ValueError("slice_axis must be 'i', 'j', or 'k'")
     if slice_index is None:
         slice_index = scalar_field.shape[0] // 2  # Default to the middle slice
 
-    if slice_axis == 'x':
+    if slice_axis == 'i':
         slice_data = scalar_field[slice_index, :, :]
-        xlabel, ylabel = 'Y', 'Z'
-    elif slice_axis == 'y':
+        xlabel, ylabel = r'$\theta$', r'$\phi$'
+    elif slice_axis == 'j':
         slice_data = scalar_field[:, slice_index, :]
-        xlabel, ylabel = 'X', 'Z'
-    else:  # 'z'
+        xlabel, ylabel = r'$r$', r'$\phi$'
+    else:  # 'k'
         slice_data = scalar_field[:, :, slice_index]
-        xlabel, ylabel = 'X', 'Y'
+        xlabel, ylabel = r'$r$', r'$\theta$'
 
-    fig, ax = plt.subplots(**kwargs)
+    fig, ax = plt.subplots(**kwargs, subplot_kw={'projection': 'polar'})
     img = ax.imshow(slice_data, origin='lower', cmap='viridis', aspect='auto')
     plt.colorbar(img, label='Scalar Field Value')
     ax.set_xlabel(xlabel)
