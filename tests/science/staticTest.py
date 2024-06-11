2669ad85f5076bf0e7fa8950fc6b55fb7f6f9965
Emily Boudreaux <emily@boudreauxmail.com>
2024-06-11 19:12:47 +0000

    test(staticTest.py): quick integration / science test added

--- tests/science/staticTest.py
+++ tests/science/staticTest.py
@@ -1,3 +1,25 @@
-Y0292EOS = EOS("../../EOS/TABLEEOS_2021_Trho_Y0292_v1")
-opacity = KramerOpac(0.70, 0.02)
-sphere = VoxelSphere(8e31, 15, "../../BrownDwarfMESA/BD_TEST.mod", Y0292EOS, opacity)
+from CoolDwarf.star import VoxelSphere, default_tol
+from CoolDwarf.utils import setup_logging
+from CoolDwarf.EOS import get_eos
+from CoolDwarf.opac import KramerOpac
+
+
+
+setup_logging(debug=False)
+
+EOS = get_eos("../../../EOS/TABLEEOS_2021_Trho_Y0292_v1", "CD21")
+opac = KramerOpac(0.7, 0.02)
+tol = default_tol()
+tol['volCheck'] = 1
+sphere = VoxelSphere(
+    8e31,
+    "../../../BrownDwarfMESA/BD_TEST.mod",
+    EOS,
+    opac,
+    radialResolution=100,
+    altitudinalResolition=100,
+    azimuthalResolition=100,
+    tol=tol,
+    cfl_factor = 0.4,
+)
+sphere.evolve(maxTime = 60*60*24, pbar=False)
