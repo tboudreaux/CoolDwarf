a2b5ede94d27434b8905c67b76eac9c67583281f
Emily Boudreaux <emily@boudreauxmail.com>
2024-06-11 19:11:35 +0000

    feat(plot2d.py): added spherical slice visualization

--- src/CoolDwarf/utils/plot/plot2d.py
+++ src/CoolDwarf/utils/plot/plot2d.py
@@ -27,3 +27,19 @@ def visualize_scalar_field(scalar_field, slice_axis='z', slice_index=None, **kwa
     ax.set_ylabel(ylabel)
     ax.set_title(f'Slice along {slice_axis}-axis at index {slice_index}')
     return fig, ax
+
+def plot_polar_slice(sphere, data, phi_slice=0, theta_offset=0):
+    phi_idx = (np.abs(sphere.PHI[0, 0, :].get() - phi_slice)).argmin()
+   
+    r_slice = sphere.R[:, :, phi_idx].get()
+    theta_slice = sphere.THETA[:, :, phi_idx].get() + theta_offset
+    data_slice = data[:, :, phi_idx].get()
+    print(data_slice)
+    
+    
+    fig, ax = plt.subplots(figsize=(10, 7), subplot_kw={'projection': 'polar'})
+    c = ax.pcolormesh(theta_slice, r_slice, data_slice, shading='auto')
+    fig.colorbar(c, ax=ax)
+    ax.set_theta_zero_location('N')
+    ax.set_theta_direction(-1)
+    plt.savefig("polar_slice.png")
