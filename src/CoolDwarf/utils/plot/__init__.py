2c48d366b6f0d7041cdf61081b667d20b35592ec
Emily Boudreaux <emily@boudreauxmail.com>
2024-06-15 10:03:05 +0000

    refactor(plot): brought plot module in line with multiple backend support

--- src/CoolDwarf/utils/plot/__init__.py
+++ src/CoolDwarf/utils/plot/__init__.py
@@ -1,3 +1,2 @@
 from .plot3d import plot_3d_gradients
 from .plot2d import plot_polar_slice
-from .plot2d import visualize_scalar_field
--- src/CoolDwarf/utils/plot/plot2d.py
+++ src/CoolDwarf/utils/plot/plot2d.py
@@ -1,39 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def visualize_scalar_field(scalar_field, slice_axis='z', slice_index=None, **kwargs):
-    if slice_axis not in ['i', 'j', 'k']:
-        raise ValueError("slice_axis must be 'i', 'j', or 'k'")
-    if slice_index is None:
-        slice_index = scalar_field.shape[0] // 2  # Default to the middle slice
+from CoolDwarf.utils.misc.backend import get_array_module
 
-    if slice_axis == 'i':
-        slice_data = scalar_field[slice_index, :, :]
-        xlabel, ylabel = r'$\theta$', r'$\phi$'
-    elif slice_axis == 'j':
-        slice_data = scalar_field[:, slice_index, :]
-        xlabel, ylabel = r'$r$', r'$\phi$'
-    else:  # 'k'
-        slice_data = scalar_field[:, :, slice_index]
-        xlabel, ylabel = r'$r$', r'$\theta$'
+xp, CUPY = get_array_module()
 
-    if slice_axis != 'i':
-        fig, ax = plt.subplots(**kwargs, subplot_kw={'projection': 'polar'})
+def plot_polar_slice(sphere, data, phi_slice=0, theta_offset=0):
+    if CUPY:
+        phi_idx = (xp.abs(sphere.PHI[0, 0, :].get() - phi_slice)).argmin()
+        r_slice = sphere.R[:, :, phi_idx].get()
+        theta_slice = sphere.THETA[:, :, phi_idx].get() + theta_offset
+        data_slice = data[:, :, phi_idx].get()
     else:
-        fig, ax = plt.subplots(**kwargs)
-    img = ax.imshow(slice_data, origin='lower', cmap='viridis', aspect='auto')
-    plt.colorbar(img, label='Scalar Field Value')
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.set_title(f'Slice along {slice_axis}-axis at index {slice_index}')
-    return fig, ax
-
-def plot_polar_slice(sphere, data, phi_slice=0, theta_offset=0, fname="polar_slice.png"):
-    phi_idx = (np.abs(sphere.PHI[0, 0, :].get() - phi_slice)).argmin()
-   
-    r_slice = sphere.R[:, :, phi_idx].get()
-    theta_slice = sphere.THETA[:, :, phi_idx].get() + theta_offset
-    data_slice = data[:, :, phi_idx].get()
+        phi_idx = (xp.abs(sphere.PHI[0, 0, :] - phi_slice)).argmin()
+        r_slice = sphere.R[:, :, phi_idx]
+        theta_slice = sphere.THETA[:, :, phi_idx] + theta_offset
+        data_slice = data[:, :, phi_idx]
     
     
     fig, ax = plt.subplots(figsize=(10, 7), subplot_kw={'projection': 'polar'})
@@ -41,5 +23,4 @@ def plot_polar_slice(sphere, data, phi_slice=0, theta_offset=0, fname="polar_sli
     fig.colorbar(c, ax=ax)
     ax.set_theta_zero_location('N')
     ax.set_theta_direction(-1)
-    plt.savefig(fname)
-    plt.close()
+    return fig, ax
